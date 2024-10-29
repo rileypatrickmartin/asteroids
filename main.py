3d8731d29144e4c5ebe97d795cea92d63bfa60c7
rileypatrickmartin <rmartin@gatech.edu>
2024-10-29 12:52:08 -0400

    Moving player into groups for future ease of managing multiple objects

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,8 +19,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen)
+        for updatable in updatables:
+            updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
