ead7e0a707f343478e226c80967ea5dfd75b3bc1
rileypatrickmartin <rmartin@gatech.edu>
2024-10-28 20:43:13 -0400

    Limiting to 60 fps and adding circle sprite

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == '__main__':
     main()
